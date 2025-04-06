03909294d4699b379e3d1b9e1473c27dfd58ff55
Ivan Kosach <kosaci74@gmail.com>
2025-04-06 16:20:28 +0300

    fix

--- main.py
+++ main.py
@@ -55,8 +55,8 @@ if DISPLAY:
 running = True
 while running:
     if not crossword or AUTO_RESOLUTION and crossword.finished:
-        if num_i >= len(nums):
-            nums += get_numbers(driver, n=len(nums) - num_i + 1)
+        while num_i >= len(nums):
+            nums += get_numbers(driver, n=num_i - len(nums) + 1)
         while True:
             try:
                 rows, cols, rows_colors, cols_colors, colors, deep = get_puzzle(driver, nums[num_i])
@@ -67,7 +67,7 @@ while running:
                 time.sleep(3)
                 num_i += 1
                 if num_i >= len(nums):
-                    nums += get_numbers(driver, n=len(nums) - num_i + 1)
+                    nums += get_numbers(driver, n=num_i - len(nums) + 1)
 
         screen, a = None, None
         if DISPLAY:
